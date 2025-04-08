dec7f95b2409da0f81a7b3299f585f41b7c8f404
lumi <skawogus3358@gmail.com>
2025-04-09 08:29:28 +0900

    04-08 메인-심플프래그먼트-디테일액티비티 작업 추가

--- app/src/main/java/com/example/a0401chkmyplan/BottomSheet.kt
+++ app/src/main/java/com/example/a0401chkmyplan/BottomSheet.kt
@@ -1,10 +1,12 @@
 package com.example.a0401chkmyplan
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.a0401chkmyplan.databinding.FragmentBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,44 +19,30 @@ private const val ARG_PARAM2 = "param2"
  * Use the [BottomSheet.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BottomSheet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class BottomSheet : BottomSheetDialogFragment(){
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentBottomSheetBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+        _binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BottomSheet.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BottomSheet().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mainBsTimeSet.setOnClickListener {
+            val intent = Intent(requireActivity(), ActivityDetailSet::class.java)
+            startActivity(intent)
+        }
     }
 }
