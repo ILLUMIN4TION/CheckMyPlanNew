20ead3a8423f238235cf1ac18f1ab76b98f4a291
sjlee-02 <sangjine2002@gmail.com>
2025-04-09 00:37:15 +0900

    채크박스에 채크시 오늘할일이 완료된일로 채크된상태로이동하게 수정. +아이콘을 추가해서 누를수있는 버튼형식으로 추가

--- app/src/main/java/com/example/a0401chkmyplan/MainActivity.kt
+++ app/src/main/java/com/example/a0401chkmyplan/MainActivity.kt
@@ -18,19 +18,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+// todayBox1: 첫 번째 할 일 박스의 뷰를 변수에 저장
         val todayBox1 = binding.todayBox1
+
+// todayCheck1: 해당 박스 안에 있는 체크박스 뷰를 변수에 저장
         val todayCheck1 = binding.todayCheck1
+
+// todayList: '오늘 할 일' 리스트(LinearLayout)를 변수에 저장
         val todayList = binding.todayListLayout
+
+// doneList: '완료된 일' 리스트(LinearLayout)를 변수에 저장
         val doneList = binding.doneListLayout
 
-
+// 체크박스의 체크 상태가 바뀔 때 실행되는 리스너 등록
         todayCheck1.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                // 체크되면(todayBox1을) '오늘 할 일' 리스트에서 제거하고,
+                // '완료된 일' 리스트에 추가
                 todayList.removeView(todayBox1)
                 doneList.addView(todayBox1)
             } else {
+                // 체크 해제되면(todayBox1을) '완료된 일' 리스트에서 제거하고,
+                // '오늘 할 일' 리스트 맨 위에 다시 추가
                 doneList.removeView(todayBox1)
-                todayList.addView(todayBox1, 0)
+                todayList.addView(todayBox1, 0)  // 0: 리스트 제일 위에 추가
             }
         }
 
